25bc0ac6a96894efebd8fa9122e9df1456b7b421
Duncan Calvert <dcalvert@ihmc.us>
2018-05-18 12:11:36 -0500

    Temp ros2 experimental changes.

---
+++ atlas/src/test/java/us/ihmc/atlas/AtlasKinematicsToolboxDiagnosticsTest.java
@@ -0,0 +1,43 @@
+package us.ihmc.atlas;
+
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.junit.Test;
+
+import controller_msgs.msg.dds.RobotConfigurationData;
+import us.ihmc.atlas.AtlasRobotModel;
+import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.avatar.drcRobot.RobotTarget;
+import us.ihmc.avatar.networkProcessor.kinematicsToolboxModule.KinematicToolboxDiagnosticEnvironment;
+import us.ihmc.commons.PrintTools;
+import us.ihmc.communication.packetCommunicator.PacketCommunicator;
+import us.ihmc.communication.util.NetworkPorts;
+import us.ihmc.humanoidRobotics.kryo.IHMCCommunicationKryoNetClassList;
+
+public class AtlasKinematicsToolboxDiagnosticsTest
+{
+   @Test(timeout = 30000)
+   public void testReceiveRobotConfigurationData()
+   {
+      AtlasRobotVersion atlasVersion = AtlasRobotVersion.ATLAS_UNPLUGGED_V5_DUAL_ROBOTIQ;
+      RobotTarget target = RobotTarget.SCS;
+      boolean headless = true;
+      AtlasRobotModel atlasModel = new AtlasRobotModel(atlasVersion, target, headless);
+
+      KinematicToolboxDiagnosticEnvironment kinematicToolboxDiagnosticEnvironment = new KinematicToolboxDiagnosticEnvironment(atlasModel);
+
+      PacketCommunicator controllerPacketCommunicator = PacketCommunicator.createIntraprocessPacketCommunicator(NetworkPorts.CONTROLLER_PORT,
+                                                                                                                new IHMCCommunicationKryoNetClassList());
+      MutableInt receivedMessages = new MutableInt();
+      controllerPacketCommunicator.attachListener(RobotConfigurationData.class, packet -> {
+         PrintTools.info("Received: " + receivedMessages.intValue() + " joints: " + packet.getJointAngles().size());
+         receivedMessages.increment();
+      });
+
+      
+      while (receivedMessages.intValue() < 10)
+      {
+         Thread.yield();
+      }
+      
+   }
+}
