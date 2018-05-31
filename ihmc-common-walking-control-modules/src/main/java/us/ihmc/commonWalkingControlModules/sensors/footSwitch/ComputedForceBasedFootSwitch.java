09abba974baff503bc168caf7118d6dd3a107f1b
brandon shrewsbury <brandon.shrewsbury@gmail.com>
2018-05-31 10:49:45 -0700

    ComputedForceBasedFootSwitch now implements FootSwitchInterface and can handle different kinematic chains between the CoM and the sole. Added a glitch filter to the contact variable

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/sensors/footSwitch/ComputedForceBasedFootSwitch.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/sensors/footSwitch/ComputedForceBasedFootSwitch.java
@@ -1,91 +1,168 @@
-package us.ihmc.commonWalkingControlModules.touchdownDetector;
+package us.ihmc.commonWalkingControlModules.sensors.footSwitch;
 
-import java.util.List;
-
-import org.ejml.alg.dense.misc.UnrolledInverseFromMinor;
 import org.ejml.data.DenseMatrix64F;
+import org.ejml.factory.LinearSolverFactory;
+import org.ejml.interfaces.linsol.LinearSolver;
 import org.ejml.ops.CommonOps;
 
+import us.ihmc.euclid.referenceFrame.FramePoint2D;
 import us.ihmc.euclid.referenceFrame.FrameVector3D;
 import us.ihmc.euclid.referenceFrame.ReferenceFrame;
-import us.ihmc.robotModels.FullQuadrupedRobotModel;
+import us.ihmc.robotModels.FullLeggedRobotModel;
+import us.ihmc.robotics.math.filters.GlitchFilteredYoBoolean;
+import us.ihmc.robotics.robotSide.RobotSegment;
+import us.ihmc.robotics.screwTheory.GeometricJacobian;
+import us.ihmc.robotics.screwTheory.MovingReferenceFrame;
+import us.ihmc.robotics.screwTheory.OneDoFJoint;
+import us.ihmc.robotics.screwTheory.RigidBody;
+import us.ihmc.robotics.screwTheory.ScrewTools;
+import us.ihmc.robotics.screwTheory.Wrench;
+import us.ihmc.robotics.sensors.FootSwitchInterface;
+import us.ihmc.yoVariables.providers.DoubleProvider;
 import us.ihmc.yoVariables.registry.YoVariableRegistry;
 import us.ihmc.yoVariables.variable.YoBoolean;
 import us.ihmc.yoVariables.variable.YoDouble;
-import us.ihmc.robotics.linearAlgebra.MatrixTools;
-import us.ihmc.robotics.robotSide.RobotQuadrant;
-import us.ihmc.robotics.screwTheory.GeometricJacobian;
-import us.ihmc.robotics.screwTheory.OneDoFJoint;
-import us.ihmc.robotics.screwTheory.RigidBody;
 
-public class ForceBasedTouchDownDetection implements TouchdownDetector
+/**
+ * Computes the wrench at the sole frame using the Jacobian from Root to Sole and 
+ * compares the linear Z force in World to the zForceThreshold to determine if the foot can be trusted
+ * This class is useful if you don't have a contact sensor in the foot
+ */
+public class ComputedForceBasedFootSwitch<E extends Enum<E> & RobotSegment<E>> implements FootSwitchInterface
 {
    private final String name = getClass().getSimpleName();
-   private final YoVariableRegistry registry ;
-   private final GeometricJacobian footJacobian;
-   private final DenseMatrix64F linearPartOfJacobian = new DenseMatrix64F(3, 3);   
-   private final DenseMatrix64F linearJacobianInverse = new DenseMatrix64F(3, 3);
-   private final DenseMatrix64F selectionMatrix = CommonOps.identity(6);   
-   private final DenseMatrix64F jointTorques = new DenseMatrix64F(3, 1);
-   private final DenseMatrix64F footLinearForce = new DenseMatrix64F(3, 1);
-   private final List<OneDoFJoint> legOneDoFJoints;
+   private final YoVariableRegistry registry;
+   private final GeometricJacobian jacobian;
+   private final OneDoFJoint[] jointsFromRootToSole;
+
+   private final DenseMatrix64F jacobianInverse;
+   private final DenseMatrix64F footWrench = new DenseMatrix64F(6, 1);
+   private final DenseMatrix64F jointTorques;
    
-   private final YoBoolean isInContact;
-   private final YoDouble zForceThreshold;
+   private final GlitchFilteredYoBoolean isInContact;
+   
+   private final DoubleProvider contactThresholdForce;
+   private final YoBoolean pastThreshold;
    private final YoDouble measuredZForce;
    private final FrameVector3D footForce = new FrameVector3D();
+   private final Wrench wrench = new Wrench();
    
-   public ForceBasedTouchDownDetection(FullQuadrupedRobotModel robotModel, RobotQuadrant robotQuadrant, ReferenceFrame soleFrame, YoVariableRegistry parentRegistry)
+   private final LinearSolver<DenseMatrix64F> solver = LinearSolverFactory.pseudoInverse(true);
+   private final double standingZForce;
+   private MovingReferenceFrame soleFrame;
+   
+   public ComputedForceBasedFootSwitch(FullLeggedRobotModel<E> robotModel, E robotSegment, DoubleProvider contactThresholdForce, YoVariableRegistry parentRegistry)
    {
-      String prefix = robotQuadrant.getCamelCaseName() + name;
+      String prefix = robotSegment.toString() + name;
       registry = new YoVariableRegistry(prefix);
       
-      isInContact = new YoBoolean(prefix + "isInContact", registry);
-      zForceThreshold = new YoDouble(prefix + "zForceThreshold", registry);
-      measuredZForce = new YoDouble(prefix + "measuredZForce", registry);
+      int filterWindowSize = 3;
       
-      zForceThreshold.set(80.0);
+      measuredZForce = new YoDouble(prefix + "measuredZForce", registry);
+      pastThreshold = new YoBoolean(prefix + "PastFootswitchThreshold", registry);
+      
+      this.contactThresholdForce = contactThresholdForce;
+      this.isInContact = new GlitchFilteredYoBoolean(prefix + "IsInContact", registry, pastThreshold, filterWindowSize);
       
       RigidBody body = robotModel.getRootBody();
-      RigidBody foot = robotModel.getFoot(robotQuadrant);
-      footJacobian = new GeometricJacobian(body, foot, soleFrame);
+      RigidBody foot = robotModel.getFoot(robotSegment);
+      soleFrame = robotModel.getSoleFrame(robotSegment);
+      jacobian = new GeometricJacobian(body, foot, soleFrame);
       
-      legOneDoFJoints = robotModel.getLegJointsList(robotQuadrant);
+      jointsFromRootToSole = ScrewTools.createOneDoFJointPath(body, foot);
       
-      //remove angular part
-      MatrixTools.removeRow(selectionMatrix, 0);
-      MatrixTools.removeRow(selectionMatrix, 0);
-      MatrixTools.removeRow(selectionMatrix, 0);
+      jointTorques = new DenseMatrix64F(jointsFromRootToSole.length, 1);
+      jacobianInverse = new DenseMatrix64F(jointsFromRootToSole.length, 3);
       
       parentRegistry.addChild(registry);
+      
+      double totalMass = robotModel.getTotalMass();
+      standingZForce = totalMass * 9.81;
+   }
+
+   public void update()
+   {
+      for(int i = 0; i < jointsFromRootToSole.length; i++)
+      {
+         OneDoFJoint oneDoFJoint = jointsFromRootToSole[i];
+         jointTorques.set(i, 0, oneDoFJoint.getTauMeasured());
+      }
+      
+      jacobian.compute();
+
+      solver.setA(jacobian.getJacobianMatrix());
+      solver.invert(jacobianInverse);
+      
+      CommonOps.multTransA(jacobianInverse, jointTorques, footWrench);
+      wrench.set(jacobian.getJacobianFrame(), footWrench);
+      
+      footForce.setToZero(jacobian.getJacobianFrame());
+      wrench.getLinearPart(footForce);
+      footForce.changeFrame(ReferenceFrame.getWorldFrame());
+      
+      measuredZForce.set(footForce.getZ() * -1.0);
+      pastThreshold.set(measuredZForce.getDoubleValue() > contactThresholdForce.getValue());
+      isInContact.update();
    }
 
    @Override
-   public boolean hasTouchedDown()
+   public boolean hasFootHitGround()
+   {
+      update();
+      return isInContact.getBooleanValue();
+   }
+
+   @Override
+   public double computeFootLoadPercentage()
+   {
+      return Math.min(1.0, measuredZForce.getDoubleValue() / standingZForce);
+   }
+
+   @Override
+   public void computeAndPackCoP(FramePoint2D copToPack)
+   {
+      copToPack.setToZero(soleFrame);
+   }
+
+   @Override
+   public void updateCoP()
+   {
+      
+   }
+
+   @Override
+   public void computeAndPackFootWrench(Wrench footWrenchToPack)
+   {
+      footWrenchToPack.set(wrench);
+   }
+
+   @Override
+   public ReferenceFrame getMeasurementFrame()
+   {
+      return jacobian.getJacobianFrame();
+   }
+
+   @Override
+   public void reset()
+   {
+      isInContact.set(false);
+   }
+
+   @Override
+   public boolean getForceMagnitudePastThreshhold()
    {
       return isInContact.getBooleanValue();
    }
 
    @Override
-   public void update()
+   public void setFootContactState(boolean hasFootHitGround)
    {
-      for(int i = 0; i < legOneDoFJoints.size(); i++)
-      {
-         OneDoFJoint oneDoFJoint = legOneDoFJoints.get(i);
-         jointTorques.set(i, 0, oneDoFJoint.getTauMeasured());
-      }
-      
-      footJacobian.compute();
-      DenseMatrix64F jacobianMatrix = footJacobian.getJacobianMatrix();
-      CommonOps.mult(selectionMatrix, jacobianMatrix, linearPartOfJacobian);
-      UnrolledInverseFromMinor.inv3(linearPartOfJacobian, linearJacobianInverse, 1.0);
-      
-      CommonOps.multTransA(linearJacobianInverse, jointTorques, footLinearForce);
-      
-      footForce.setToZero(footJacobian.getJacobianFrame());
-      footForce.set(footLinearForce.get(0), footLinearForce.get(1), footLinearForce.get(2));
-      footForce.changeFrame(ReferenceFrame.getWorldFrame());
-      measuredZForce.set(footForce.getZ() * -1.0);
-      isInContact.set(measuredZForce.getDoubleValue() > zForceThreshold.getDoubleValue());
+      isInContact.set(hasFootHitGround);
+   }
+
+   @Override
+   public void trustFootSwitch(boolean trustFootSwitch)
+   {
+      isInContact.set(trustFootSwitch);
    }
 }
