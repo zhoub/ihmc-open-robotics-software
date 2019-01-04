a083988520fad2181350471a0f84d0b464087a0f
Robert <rgriffin@ihmc.us>
2019-01-04 11:47:04 -0600

    decreased the min normal so that the vis graph test passes

--- ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/DefaultVisibilityGraphParameters.java
+++ ihmc-path-planning/src/main/java/us/ihmc/pathPlanning/visibilityGraphs/DefaultVisibilityGraphParameters.java
@@ -16,7 +16,7 @@ public class DefaultVisibilityGraphParameters implements VisibilityGraphsParamet
    @Override
    public double getNormalZThresholdForAccessibleRegions()
    {
-      return 0.75;
+      return 0.5;
    }
 
    /** {@inheritDoc} */
